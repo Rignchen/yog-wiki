10cefdb02031fd682d6e0fb221b520327bb9435f
Rignchen <rinchendemort@gmail.com>
2026-02-04 13:55:33 +0100

    build: prerender pages for every role

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+import { Roles } from '#services/roles';
+import { inject } from '@angular/core';
+
 export const serverRoutes: ServerRoute[] = [
+	{
+		path: 'role/:name',
+		renderMode: RenderMode.Prerender,
+		getPrerenderParams: async () => {
+			const roles = inject(Roles);
+			return roles.list.map(role => ({
+				name: role.name
+			}));
+		}
+	},
 	{
 		path: '**',
 		renderMode: RenderMode.Prerender,
