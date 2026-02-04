de883bd88463fc046d180e4bb3588836e731a4c8
Rignchen <rinchendemort@gmail.com>
2026-02-04 11:12:58 +0100

    feat: create role-details component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from '#pages/home';
+import { RoleDetails } from '#pages/role-details';
 
 export const routes: Routes = [
 	{path: '', component: Home},
+	{path: 'role/:name', component: RoleDetails}
 ];
