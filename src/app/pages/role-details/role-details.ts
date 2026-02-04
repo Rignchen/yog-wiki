db8be539ee7c4c728fa505edef46da0d5b9d731a
Rignchen <rinchendemort@gmail.com>
2026-02-04 15:21:46 +0100

    feat: display everything on card details page

--- src/app/pages/role-details/role-details.ts
+++ src/app/pages/role-details/role-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Roles } from '#services/roles';
+import { Role as RoleType } from '#types/role';
 
 @Component({
 	selector: 'app-role-details',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
 	templateUrl: './role-details.html',
 	styleUrl: './role-details.css',
 })
-export class RoleDetails {}
+export class RoleDetails {
+	public role: RoleType | undefined;
+	roleService = inject(Roles);
+
+	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		this.route.params.subscribe((params) => {
+			const name = params['name'];
+			this.role = this.roleService.list.find(
+				(role) => role.name.toLowerCase() === name,
+			);
+		});
+	}
+}
