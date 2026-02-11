f4a965aecdd554fc2fb20e35ed9708149ba15ef4
Rignchen <rinchendemort@gmail.com>
2026-02-11 22:35:49 +0100

    test: role not found

--- src/app/pages/role-details/role-details.spec.ts
+++ src/app/pages/role-details/role-details.spec.ts
@@ -1,28 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 
+import { provideRouter } from '@angular/router';
 import { RoleDetails } from './role-details';
 
 describe('RoleDetails', () => {
 	let component: RoleDetails;
 	let fixture: ComponentFixture<RoleDetails>;
+	let harness: RouterTestingHarness;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [RoleDetails],
 			providers: [
-				{
-					provide: ActivatedRoute,
-					useValue: {
-						params: {
-							subscribe: (callback: any) => {
-								callback({ id: 'test-role-id' });
-							},
-						},
-					},
-				},
-			],
+				provideRouter([{ path: 'roles/:name', component: RoleDetails }]),
+			]
 		}).compileComponents();
+		harness = await RouterTestingHarness.create();
 
 		fixture = TestBed.createComponent(RoleDetails);
 		component = fixture.componentInstance;
@@ -31,5 +25,12 @@ describe('RoleDetails', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should show "Role not found" if role does not exist', () => {
+		harness.navigateByUrl('/roles/nonexistent');
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.textContent).toContain('Rôle non trouvé.');
 	});
 });
